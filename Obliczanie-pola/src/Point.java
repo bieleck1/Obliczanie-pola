9f6eb53e1bd7e43e4223cef7042d403c11f90f83
abc <abc@KAROL>
2015-11-29 15:00:02 +0100

    Wyznaczanie otoczki wypukłej Signed-off-by:abc <abc@KAROL>

---
+++ Obliczanie-pola/src/Point.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author abc
+ */
+public class Point implements Comparable<Point> {
+
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    @Override
+    public int compareTo(Point p) {
+        if (this.x == p.getX()) {
+            return p.getY() - this.y;
+        } else {
+            return p.getX() - this.x;
+        }
+    }
+}
